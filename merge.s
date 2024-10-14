856a6ac7aa4da996b35716991235aab15b560507
Silas van der Waal <silasvanderwaal@gmail.com>
2024-10-14 16:00:07 +0200

    Backup

--- merge.s
+++ merge.s
@@ -1,12 +1,10 @@
-.data
-buffer_array: .space 40
+	.data
 
 	.globl merge
 	.text
 	# a0 -> Start address array
 	# a1 -> Size of the array
 merge: 
-	#Moving the stackpointer down 8 addresses
 	subu $sp, $sp, 28
 	#Saving arguments from previous rutin
 	sw $s0, 0($sp)
@@ -17,47 +15,55 @@ merge:
 	sw $s5, 20($sp)
 	sw $ra, 24($sp)
 	
-	srl $s0, $a1, 1		#s0 = size / 2 -> int half
+	sll $t0, $a1, 2
+	sub $sp, $sp, $t0
+	
+	srl $s0, $a1, 1		#s0 = size / 2
 	li $s1, 0		#s1 = i
 	add $s2, $zero, $s0	#s2 = j
 	li $s3, 0		#s3 = k
-	add $s4, $zero, $a1	#s4 = size
-	add $s5, $zero, $a0	#s5 = a[]
-	li $t0, 0		#t0 = current adderss in a[]
+	move $s4, $a1		#s4 = size
+	move $s5, $a0		#s5 = a[]
+	li $t0, 0		#t0 = Adress buffer
 
 for_loop:
-	bge $s1, $s4, end_for_loop	#Check if i <= size
-	sll $t1, $s1, 2		#t1 = i *4
+	bge $s1, $s4, end_for_loop		#Check if i <= size
+	sll $t1, $s1, 2				#t1 = i *4
 	add $t0, $s5, $t1
-	lw $t2, ($t0)		#t2 = a[i]	
-	sw $t2, buffer_array($t1)	#Save t2 to b[i]
-	addi $s1, $s1, 1	#i++
+	lw $t2, ($t0)				#t2 = a[i]
+	add $t0, $sp, $t1
+	sw $t2, ($t0)				#Save t2 to b[i]
+	addi $s1, $s1, 1			#i++
 	j for_loop
 	
 end_for_loop:
 	li $s1, 0
 	
 while_loop:
-	bge $s1, $s0, end_while_loop		#Checking while loop conditions 
-	bge $s2, $s4, end_while_loop
+	bge $s1, $s0, while_loop_2		#Checking while loop conditions 
+	bge $s2, $s4, while_loop_2
 	
 	sll $t0, $s1, 2
-	lw $t1, buffer_array($t0)		#Loading in b[i]
+	add $t0, $t0, $sp
+	lw $t1, ($t0)			#Loading in b[i]
 	sll $t0, $s2, 2
-	lw $t2, buffer_array($t0)		#loading in b[j]
+	add $t0, $t0, $sp
+	lw $t2, ($t0)			#loading in b[j]
 	bgt $t1, $t2, if		#if(b[j] <= b[j]) go to if
 	
 else:
 	sll $t0, $s2, 2
-	lw $t1, buffer_array($t0)		#load b[j]
+	add $t0, $t0, $sp
+	lw $t1, ($t0)			#load b[j]
 	sll $t0, $s3, 2
 	add $t0, $t0, $s5
 	sw $t1, ($t0)			#save b[j] to a[k]
 	addi $s2, $s2, 1		#j++
 	j k_increment
 if:
-	sll $t0, $s1, 2			
-	lw $t1, buffer_array($t0)		#Load b[i]
+	sll $t0, $s1, 2	
+	add $t0, $t0, $sp		
+	lw $t1, ($t0)		#Load b[i]
 	sll $t0, $s3, 2
 	add $t0, $t0, $s5
 	sw $t1, ($t0)			#save b[i] to a[k]
@@ -70,20 +76,22 @@ k_increment:
 end_while_loop:
 while_loop_2:
 	bge $s1, $s0, end_while_loop_2		#Branch if i is equal or greather than half
-	sll $t0, $s1, 2
-	lw $t1, buffer_array($t0)
-	sll $t0, $s3, 2
-	add $t0, $t0, $s5
-	sw $t1, ($t0)
-	addi $s1, $s1, 1
-	addi $s3, $s3, 1
+	sll $t0, $s1, 2				#i * 4
+	add $t0, $t0, $sp			#add i*4 + stack pointer
+	lw $t1, ($t0)				#Load vlaue from stack at index i
+	sll $t0, $s3, 2				#j * 4
+	add $t0, $t0, $s5			# a[k]
+	sw $t1, ($t0)				#Load value from a[k]
+	addi $s1, $s1, 1			#i++
+	addi $s3, $s3, 1			#k++	
 	j while_loop_2
 
 end_while_loop_2:
 while_loop_3:
 	bge $s2, $s4, end_while_loop_3		#Branch if i is equal or greather than size
-	sll $t0, $s2, 2
-	lw $t1, buffer_array($t0)
+	sll $t0, $s2, 2				#t0 = j * 4
+	add $t0, $t0, $sp			#add t0 to stack pointer
+	lw $t1, ($t0)				
 	sll $t0, $s3, 2
 	add $t0, $t0, $s5
 	sw $t1, ($t0)
@@ -91,6 +99,9 @@ while_loop_3:
 	addi $s3, $s3, 1
 	j while_loop_3
 end_while_loop_3:
+	#Move up stack pointer for b[]
+	sll $t0, $a1, 2
+	addu $sp, $sp, $t0
 	#Restoring the state before the rutin
 	lw $s0, 0($sp)
 	lw $s1, 4($sp)
--- mergeSort.s
+++ mergeSort.s
@@ -1,37 +1,39 @@
-	.globl mergeSort
+.globl mergeSort
 	.text
 mergeSort:
 	#Saving the start address and the size
-	subu $sp, $sp, 8
-	sw $a0, 0($sp)
-	sw $a1, 4($sp)
-	ble $a1, 1, exit_loop	#if size > 1
-	srl $t1, $a1, 1		#t1 = size / 2 
-	lw $a0, 0($sp)		#Loading arguments
-	move $a1, $t1	
-	subu $sp, $sp, 4	#Saving the stack pointer	
-	sw $ra, ($sp)	-
 	
-	jal mergeSort		#Calling mergeSort(a, half)
-	lw $ra, 0($sp)		#Loading previous stackpointer 		
+mergeSort_loop:
+	ble $a1, 1, exit_loop		#if size > 1
+	move $s0, $a0
+	move $s1, $a1			
+	srl $s3, $s1, 1			#s3 = size / 2 
+	move $a1, $s3
+	move $a0, $s0
+		
+	subu $sp, $sp, 4		#Saving the stack pointer	
+	sw $ra, ($sp)
+	jal mergeSort_loop		#Calling mergeSort(a, half)
+	lw $ra, 0($sp)			#Loading previous stackpointer 		
 	addu $sp, $sp, 4
 	
-	lw $a0, 0($sp)
-	sll $t1, $t1, 2
-	add $a0, $a0, $t1	#Calculating a + half	
-	sub $a1, $a1, $t1	#Calculating size - half
+	sll $t1, $s3, 2
+	add $a0, $s0, $t3		#Calculating a + half	
+	sub $a1, $s1, $s3		#Calculating size - half
+	
 	subu $sp, $sp, 4	
-	sw $ra, ($sp)		#Saving stack pointer             
-	jal mergeSort		#Calling mergeSort(a + half, size - half)
-	lw $ra, 0($sp)		#Loading previous stack pointer
+	sw $ra, ($sp)			#Saving stack pointer             
+	jal mergeSort_loop		#Calling mergeSort(a + half, size - half)
+	lw $ra, 0($sp)			#Loading previous stack pointer
 	addu $sp, $sp, 4	
 	
-	subu $sp, $sp, 4	#Saving stack pointer
+	move $a0, $s0
+	move $a1, $s1
+	subu $sp, $sp, 4		#Saving stack pointer
 	sw $ra, ($sp)
 	jal merge
-	lw $ra, 0($sp)		#Loading previous stack pointer
+	lw $ra, ($sp)			#Loading previous stack pointer
 	addu $sp, $sp, 4
 	
 exit_loop:
-	addu $sp, $sp, 8
-	jr $ra		#Jump back to previous pointer
+	jr $ra				#Jump back
